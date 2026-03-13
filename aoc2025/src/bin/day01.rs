0e49cd36e5205d3fd8dc91578047ca016d2a6027
moxkxs <pby5ws@virginia.edu>
2026-03-13 11:10:21 -0400

    q1p2 2025

--- aoc2025/src/bin/day01.rs
+++ aoc2025/src/bin/day01.rs
@@ -2,7 +2,6 @@ use std::fs;
 
 fn main() {
     let input_path = "input/day01.txt";
-
     let instructions = fs::read_to_string(input_path).expect("Day01.txt not found in input dir");
 
     let mut start_pos = 50;
@@ -13,16 +12,26 @@ fn main() {
             .parse::<isize>()
             .expect("Couldn't parse rotational value");
 
+
         match i.chars().next() {
-            Some('L') => start_pos += -1 * curr_move,
-            Some('R') => start_pos += curr_move,
+            Some('L') => {
+                start_pos += -1 * curr_move;
+            },
+            Some('R') => {
+                start_pos += curr_move;
+            },
             _ => panic!("The input is out of format"),
+        }
+       
+        if start_pos < 0 {
+            zero_count += start_pos / (-100) + 1;
+        } else if start_pos > 100 {
+            zero_count += start_pos / 100;
+        } else if start_pos == 0 {
+            zero_count += 1;
         }
 
         start_pos = start_pos.rem_euclid(100);
-        if start_pos == 0 {
-            zero_count += 1;
-        }
     }
 
     println!("{zero_count}");
